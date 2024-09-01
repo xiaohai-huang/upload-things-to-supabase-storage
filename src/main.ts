9577dbb1debb681244db79ad75d7e163a06ddd8a
xiaohai-huang <jeromehuang18@gmail.com>
2024-09-01 03:59:46 +0000

    Use tus to upload files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import * as core from '@actions/core'
-import path from 'path'
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-import { getFiles, getMimeType, removeDirectoryPath } from './utils'
-import { readFile } from 'fs/promises'
+import { uploadDirectory } from './utils'
 
 /**
  * The main function for the action.
@@ -17,57 +14,21 @@ export async function run(): Promise<void> {
     const sourceDir = core.getInput('SOURCE_DIR')
     const targetDir = core.getInput('TARGET_DIR')
 
-    const supabase = createClient(supabaseUrl, supabaseKey)
-    await uploadDirectory(supabase, sourceDir, bucketId, targetDir)
+    await uploadDirectory(
+      supabaseUrl,
+      supabaseKey,
+      bucketId,
+      sourceDir,
+      targetDir
+    )
 
     core.setOutput(
       'message',
       `Successfully uploaded the directory: ${sourceDir} to your supabase storage.`
     )
   } catch (error) {
+    console.error(error)
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
-}
-
-async function uploadFileToSupabase(
-  supabase: SupabaseClient,
-  bucket: string,
-  filePath: string,
-  targetPath: string
-): Promise<void> {
-  const file = await readFile(filePath)
-  const contentType = getMimeType(filePath)
-  const { error } = await supabase.storage
-    .from(bucket)
-    .upload(targetPath, file, {
-      cacheControl: '3600',
-      upsert: true,
-      contentType
-    })
-
-  if (error) {
-    throw new Error(`Failed to upload ${targetPath}: ${error.message}`)
-  } else {
-    console.log(`Successfully uploaded ${targetPath}`)
-  }
-}
-
-async function uploadDirectory(
-  supabase: SupabaseClient,
-  directoryPath: string,
-  bucket: string,
-  supabasePath: string
-): Promise<void> {
-  const files = await getFiles(directoryPath)
-  const uploadPromises = files.map(async filePath => {
-    const remotePath = path.join(
-      supabasePath,
-      removeDirectoryPath(filePath, directoryPath)
-    )
-
-    await uploadFileToSupabase(supabase, bucket, filePath, remotePath)
-  })
-
-  await Promise.all(uploadPromises)
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 import { readdir } from 'fs/promises'
 import path from 'path'
+import { createReadStream } from 'fs'
 // @ts-expect-error This package does not have ts definitions
 import { getMIMEType } from 'node-mime-types'
+import { Upload } from 'tus-js-client'
 
 export async function getFiles(directoryPath: string): Promise<string[]> {
   const files: string[] = []
@@ -32,4 +34,77 @@ export function removeDirectoryPath(
 
 export function getMimeType(filePath: string): string {
   return getMIMEType(filePath)
+}
+
+async function uploadFile(
+  supabaseUrl: string,
+  supabaseKey: string,
+  bucket: string,
+  sourcePath: string,
+  targetPath: string
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const file = createReadStream(sourcePath)
+    const contentType = getMimeType(sourcePath)
+
+    const upload = new Upload(file, {
+      endpoint: `${supabaseUrl}/storage/v1/upload/resumable`,
+      retryDelays: [0, 200, 500, 1500, 3000, 5000],
+      headers: {
+        authorization: `Bearer ${supabaseKey}`,
+        'x-upsert': 'true' // optionally set upsert to true to overwrite existing files
+      },
+      uploadDataDuringCreation: true,
+      removeFingerprintOnSuccess: true, // Important if you want to allow re-uploading the same file https://github.com/tus/tus-js-client/blob/main/docs/api.md#removefingerprintonsuccess
+      metadata: {
+        bucketName: bucket,
+        objectName: targetPath,
+        contentType,
+        cacheControl: '3600'
+      },
+      chunkSize: 6 * 1024 * 1024, // NOTE: it must be set to 6MB (for now) do not change it
+      onError(error) {
+        console.error(`Failed to upload ${sourcePath} because: ${error}`)
+        reject(error)
+      },
+      onProgress(bytesUploaded, bytesTotal) {
+        const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2)
+        console.log(`Uploading ${sourcePath} ${percentage}%`)
+      },
+      onSuccess() {
+        console.log(`Successfully uploaded ${sourcePath}`)
+        resolve()
+      }
+    })
+
+    // Check if there are any previous uploads to continue.
+    upload.findPreviousUploads().then(previousUploads => {
+      // Found previous uploads so we select the first one.
+      if (previousUploads.length) {
+        upload.resumeFromPreviousUpload(previousUploads[0])
+      }
+
+      upload.start()
+    })
+  })
+}
+
+export async function uploadDirectory(
+  supabaseUrl: string,
+  supabaseKey: string,
+  bucket: string,
+  sourceDirPath: string,
+  targetDirPath: string
+): Promise<void> {
+  const files = await getFiles(sourceDirPath)
+  const uploadPromises = files.map(async sourcePath => {
+    const targetPath = path.join(
+      targetDirPath,
+      removeDirectoryPath(sourcePath, sourceDirPath)
+    )
+
+    await uploadFile(supabaseUrl, supabaseKey, bucket, sourcePath, targetPath)
+  })
+
+  await Promise.all(uploadPromises)
 }
